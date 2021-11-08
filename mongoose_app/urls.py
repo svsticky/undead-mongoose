8d85fd15a662345e3ef8f85c874f002614ac4d39
Mstiekema <github@mstiekema.nl>
2021-11-08 22:21:41 +0100

    Added template views

--- mongoose_app/urls.py
+++ mongoose_app/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
+    # GET endpoints
+    path('/card', views.get_card, name='get_card'),
+    path('/products', views.get_products, name='get_products'),
+
+    # POST endpoints
+    path('/transaction', views.create_transaction, name='create_transaction'),
+    path('/register', views.register_card, name='register_card'),
 ]
--- mongoose_app/views.py
+++ mongoose_app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
+    return render(request, "index.html")
+
+# GET endpoints
+@require_GET()
+def get_card(request):
+    return render(request, "index.html")
+
+@require_GET()
+def get_products(request):
+    return render(request, "index.html")
+
+# POST endpoints
+@require_POST()
+def create_transaction(request):
+    return render(request, "index.html")
+
+@require_POST()
+def register_card(request):
     return render(request, "index.html")

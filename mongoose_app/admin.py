7d91c2725cee213f7897f92c5d3aca8b06cda344
Niels <niels.kwadijk@gmail.com>
2021-11-10 16:50:39 +0100

    Rename headers in admin portal

--- mongoose_app/admin.py
+++ mongoose_app/admin.py
@@ -3,6 +3,10 @@ from django.http.request import HttpRequest
 from .models import Product, Category, Transaction
 
 # Register your models here.
+
+admin.site.site_header = "Undead Mongoose Admin Platform"
+admin.site.site_title = "Undead Mongoose Admin"
+admin.site.index_title = "Welcome to Undead Mongoose"
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'euro', 'category', 'image_view')
--- mongoose_app/apps.py
+++ mongoose_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MongooseAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mongoose_app'
+    verbose_name = "Undead Mongoose"

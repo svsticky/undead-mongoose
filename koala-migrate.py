4eec7f0ec83f30bf09dc21faae3be0a0a13019a3
Rick van Hoef <rickvhoef@gmail.com>
2022-01-13 23:10:25 +0100

    migrate users (crude)

--- koala-migrate.py
+++ koala-migrate.py
@@ -21,10 +21,47 @@ def get_mongoose_connection():
     db_port = os.getenv("DB_PORT")
     return psycopg2.connect(f"dbname={db_name} user={db_user} password={db_password} host={db_host} port={db_port}")
 
-koala_cursor = get_koala_connection().cursor()
-mongoose_cursor = get_mongoose_connection().cursor()
+koala = get_koala_connection()
+mongoose = get_mongoose_connection()
 
-select_members_with_tegoed = "select * from members"
+# cur.execute('INSERT INTO %s (day, elapsed_time, net_time, length, average_speed, geometry) VALUES (%s, %s, %s, %s, %s, %s)', (escaped_name, day, time_length, time_length_net, length_km, avg_speed, myLine_ppy))
 
-koala_cursor.execute(select_members_with_tegoed)
-print(koala_cursor.fetchall())
+def migrate_users():
+    select_members_with_tegoed = """
+        select members.id, members.first_name, members.infix, members.last_name, members.birth_date, checkout_balances.balance
+        from public.members
+        inner join checkout_balances on members.id = checkout_balances.member_id;
+    """
+
+    with koala:
+        koala_cursor = koala.cursor()
+
+        koala_cursor.execute(select_members_with_tegoed)
+        
+        for user in koala_cursor.fetchall():
+            id = user[0]
+            
+            if user[2] == None:
+                name = f"{user[1]} {user[3]}"
+            else:
+                name = f"{user[1]} {user[2]} {user[3]}"
+
+            birthday = user[4]
+            balance = user[5]
+
+            create_user_with_tegoed(id, balance, name, birthday)
+
+# TODO: use execute_many to insert a whole batch at once and reuse the connection
+# TODO: delete table contents before migration
+def create_user_with_tegoed(id, balance, name, birthday):
+    query = f"""
+        insert into public.mongoose_app_user(user_id, balance, name, birthday)
+        values (%s, %s, %s, %s)
+    """
+
+    with mongoose:
+        mongoose_cursor = mongoose.cursor()
+
+        mongoose_cursor.execute(query, (id, balance, name, birthday))
+
+migrate_users()

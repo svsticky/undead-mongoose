729b41fdf94106547c3f1aec3674a3c290c26d02
Rick van Hoef <rickvhoef@gmail.com>
2022-01-14 16:09:22 +0100

    migrate cards

--- koala-migrate.py
+++ koala-migrate.py
@@ -2,8 +2,6 @@
 import psycopg2
 import os
 
-# TODO wrap database stuff in a try-block and close it in a finally block
-
 def get_koala_connection():
     db_name = os.getenv("KOALA_DB_NAME")
     db_user = os.getenv("KOALA_DB_USER")
@@ -23,8 +21,6 @@ def get_mongoose_connection():
 
 koala = get_koala_connection()
 mongoose = get_mongoose_connection()
-
-# cur.execute('INSERT INTO %s (day, elapsed_time, net_time, length, average_speed, geometry) VALUES (%s, %s, %s, %s, %s, %s)', (escaped_name, day, time_length, time_length_net, length_km, avg_speed, myLine_ppy))
 
 def migrate_users():
     select_members_with_tegoed = """
@@ -64,4 +60,54 @@ def create_user_with_tegoed(id, balance, name, birthday):
 
         mongoose_cursor.execute(query, (id, balance, name, birthday))
 
+
+def migrate_cards():
+    query = """
+        select uuid, active, member_id
+        from public.checkout_cards
+    """
+
+    with koala:
+        koala_cursor = koala.cursor()
+
+        koala_cursor.execute(query)
+        
+        for card in koala_cursor.fetchall():
+            card_id = card[0]
+            active = card[1]
+            koala_id = card[2]
+            mongoose_id = get_userid_by_koala_id(koala_id)
+
+            create_card(card_id, active, mongoose_id)
+
+
+def get_userid_by_koala_id(koala_id):
+    query = f"""
+        select id
+        from mongoose_app_user
+        where user_id = '{koala_id}'
+    """
+
+    with mongoose:
+        mongoose_cursor = mongoose.cursor()
+
+        mongoose_cursor.execute(query)
+
+        return mongoose_cursor.fetchone()
+
+
+
+def create_card(card_id, active, user_id):
+    query = f"""
+        insert into public.mongoose_app_card(card_id, active, user_id_id)
+        values (%s, %s, %s)
+    """
+
+    with mongoose:
+        mongoose_cursor = mongoose.cursor()
+
+        mongoose_cursor.execute(query, (card_id, active, user_id))
+
+
 migrate_users()
+migrate_cards()

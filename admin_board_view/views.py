30805dd3d93973da1ec915be0dd802019c275b3e
Mstiekema <github@mstiekema.nl>
2022-06-20 22:31:38 +0200

    Added template functions for add/edit/toggle/remove

--- admin_board_view/views.py
+++ admin_board_view/views.py
@@ -1,3 +1,4 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from .models import *
 
@@ -8,3 +9,40 @@ def products(request):
     products = Product.objects.all()
     categories = Category.objects.all()
     return render(request, "products.html", { "products": products, "categories": categories })
+
+# Endpoints for altering models
+def add(request):
+    body = request.POST.dict()
+
+    product = Product(
+        name=body["name"],
+        price=body["price"],
+        category=body["category"],
+        vat=body["vat"],
+        enabled=body["enabled"],
+        image=body["image"]
+    )
+
+    product.save()
+
+    return JsonResponse({ "msg": f"Added new product with id {product.id}" })
+
+def edit(request):
+    id = request.POST.dict()['id']
+    product = Product.objects.get(id=id)
+
+    # Do some product editing stuff
+    
+    return JsonResponse({ "msg": f"Edited the product with id {product.id}" })
+
+def remove(request):
+    id = request.POST.dict()['id']
+    Product.objects.get(id=id).delete()
+    return JsonResponse({ "msg": f"Removed product with {id}" })
+
+def toggle(request):
+    id = request.POST.dict()['id']
+    product = Product.objects.get(id=id)
+    product.enabled = not product.enabled
+    product.save()
+    return JsonResponse({ "msg": f"Set the state of product {id} to enabled={product.enabled}" })

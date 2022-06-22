b3ae40bee19afc5cec0f3c3104121306a7f8db9d
Mstiekema <github@mstiekema.nl>
2022-06-22 19:42:31 +0200

    Products can be toggled and deleted

--- admin_board_view/urls.py
+++ admin_board_view/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products', views.products, name='products'),
+
+    path('product/create', views.create, name='create'),
+    path('product/toggle', views.toggle, name='toggle'),
+    path('product/delete', views.delete, name='delete'),
+    path('product/edit', views.edit, name='edit'),
 ]

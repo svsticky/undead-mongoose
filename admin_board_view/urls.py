d4c18225da7bb4369099fc943c2bb8fc0c48ddf5
Mstiekema <github@mstiekema.nl>
2022-09-26 17:03:18 +0200

    Added base for transactions and settings page

--- admin_board_view/urls.py
+++ admin_board_view/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products', views.products, name='products'),
+    path('settings', views.settings, name='settings'),
+    path('transactions', views.transactions, name='transactions'),
 
     path('product/toggle', views.toggle, name='toggle'),
     path('product/delete', views.delete, name='delete'),

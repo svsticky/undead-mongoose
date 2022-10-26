a80192670e9405dfaf64bbded5a3bf68df7be4fc
Tobias de Bruijn <t.debruijn@array21.dev>
2022-10-26 19:21:08 +0200

    Double product name length

--- mongoose_app/models.py
+++ mongoose_app/models.py
@@ -28,7 +28,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=60)
     price = models.DecimalField(decimal_places=2, max_digits=6)
     image = models.ImageField(null=True)
     category = models.ForeignKey(

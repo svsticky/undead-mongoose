c253b3725d5b28feb66cff300a75762c2974431a
Mstiekema <github@mstiekema.nl>
2023-09-29 16:26:25 +0200

    Proper product redirect on interaction, removed unused code and fixed code smells

--- admin_board_view/urls.py
+++ admin_board_view/urls.py
@@ -7,12 +7,11 @@ urlpatterns = [
     path('products', views.products, name='products'),
     path('users', views.users, name='users'),
     path('users/<str:user_id>', views.users, name='user'),
-    path('settings', views.settings, name='settings'),
+    path('settings', views.settings_page, name='settings'),
     path('transactions', views.transactions, name='transactions'),
 
     path('product/toggle', views.toggle, name='toggle'),
     path('product/delete', views.delete, name='delete'),
-    path('product/edit', views.edit, name='edit'),
 
     path('category/edit', views.category, name='category'),
     path('vat/edit', views.vat, name='vat'),

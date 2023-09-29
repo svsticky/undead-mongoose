c251e0f6503748e9862252c5e4ebc64614d2d1e2
Mstiekema <github@mstiekema.nl>
2023-09-29 14:28:57 +0200

    Fixed migration ID for top up transactions

--- mongoose_app/migrations/0026_type_for_top_up_transactions_and_image_not_required.py
+++ mongoose_app/migrations/0026_type_for_top_up_transactions_and_image_not_required.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mongoose_app', '0024_product_enabled'),
+        ('mongoose_app', '0025_alter_product_name'),
     ]
 
     operations = [

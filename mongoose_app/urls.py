a6f164b4ef2902dbe86f7981ffbd98333d4d3ef8
Job Vonk <sticcpistt@icloud.com>
2024-12-17 08:46:34 +0100

    Remove index route and templates from mongoose_app

--- mongoose_app/urls.py
+++ mongoose_app/urls.py
@@ -3,20 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    
     # GET endpoints
-    path('card', views.get_card, name='get_card'),
-    path('products', views.get_products, name='get_products'),
-    path('confirm', views.confirm_card, name='confirm_card'),
-
+    path("card", views.get_card, name="get_card"),
+    path("products", views.get_products, name="get_products"),
+    path("confirm", views.confirm_card, name="confirm_card"),
     # POST endpoints
-    path('transaction', views.create_transaction, name='create_transaction'),
-    path('balance', views.update_balance, name='update_balance'),
-    path('register', views.register_card, name='register_card'),
-    path('catchwebhook', views.on_webhook, name='receive_update'),
-
-    path('topup', views.topup, name='topup'),
-    path('payment/webhook', views.payment_webhook, name='payment_webhook'),
-    path('payment/success', views.payment_success, name='payment_success')
+    path("transaction", views.create_transaction, name="create_transaction"),
+    path("balance", views.update_balance, name="update_balance"),
+    path("register", views.register_card, name="register_card"),
+    path("catchwebhook", views.on_webhook, name="receive_update"),
+    path("topup", views.topup, name="topup"),
+    path("payment/webhook", views.payment_webhook, name="payment_webhook"),
+    path("payment/success", views.payment_success, name="payment_success"),
 ]
